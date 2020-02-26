a9c6cd253ad8701426d5ef1f5bd5b25f757b550e
Dafali9012 <Daniel.falklimberg@gmail.com>
2020-02-26 15:41:44 +0100

    seems fine

--- src/Program.java
+++ src/Program.java
@@ -1,8 +1,12 @@
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Program {
     private static boolean init = false;
+    private LocalDate birthDate = LocalDate.of(1990, 12, 5);
+    ZonedDateTime skypeAppointmentNewYork = ZonedDateTime.of(LocalDateTime.of(2020, 04, 1, 14, 30), ZoneId.of("America/New_York"));
+    ZonedDateTime skypeAppointmentMalmo = skypeAppointmentNewYork.withZoneSameInstant(ZoneId.of("Europe/Stockholm"));
 
     public static void main(String[] args) {
         if (init) {
@@ -17,48 +21,7 @@ public class Program {
         }
         init = true;
         // Program starts here!
-        LocalDate birthDate = LocalDate.of(1990, 12, 5);
-
-        System.out.println("Days passed since my birthday: " + dateDifferenceDays(LocalDate.now(), birthDate));
-
-        ZonedDateTime skypeAppointmentNewYork = ZonedDateTime.of(LocalDateTime.of(2020, 04, 1, 8, 30), ZoneId.of("America/New_York"));
-        ZonedDateTime skypeAppointmentMalmo = skypeAppointmentNewYork.withZoneSameInstant(ZoneId.of("Europe/Stockholm"));
-
-        System.out.println("Skype meeting in New York at: " + DateTimeFormatter.ofPattern("MM-dd-yyyy hh:mm a").format(skypeAppointmentNewYork));
-        System.out.println("Skype meeting in Malmö at: " + DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm").format(skypeAppointmentMalmo));
-
-        System.out.println("Number of Friday the Thirteenths: " + fridayThirteenth(LocalDate.of(1900, 1, 13)));
-
-        System.out.printf("Days spent at the toilet at age 80: %d", bathroomTimeMinutes(80) / 60 / 24);
-
-        final int hoursSleep = 8;
-        final int hoursWork = 8;
-        final int hoursTravelTime = 1;
-        final double hoursToiletTime = 24 * 0.02;
-        final double hoursHygiene = 0.5;
-        final int hoursChores = 1;
-        final double hoursEat = 0.5;
-        final double hoursWeekday = hoursSleep + hoursWork + hoursTravelTime + hoursToiletTime + hoursHygiene + hoursChores + hoursEat;
-        final double hoursWeekend = hoursSleep + hoursToiletTime + hoursHygiene + hoursChores + hoursEat;
-        final double hoursEndOfLife = hoursSleep + (hoursToiletTime + hoursHygiene + hoursChores + hoursEat) * 2;
-        // Weekend no work.
-        // 65-80 no work, double everything else.
-        double freeTime = 0;
-
-        while (birthDate.getDayOfYear() != 2070 || birthDate.getMonthValue() != 12 || birthDate.getDayOfMonth() != 5) {
-            if (birthDate.getYear() >= 2055) {
-                freeTime += 24-hoursEndOfLife;
-            } else {
-                if (birthDate.getDayOfWeek() == DayOfWeek.SATURDAY || birthDate.getDayOfWeek() == DayOfWeek.SUNDAY) {
-                    freeTime += 24 - hoursWeekend;
-                } else {
-                    freeTime += 24 - hoursWeekday;
-                }
-            }
-            birthDate = birthDate.plusDays(1);
-        }
-
-        System.out.println("Percent of life freetime: "+freeTime);
+        prints();
     }
 
     private long dateDifferenceDays(LocalDate date1, LocalDate date2) {
@@ -79,7 +42,48 @@ public class Program {
         return result;
     }
 
-    private int bathroomTimeMinutes(int age) {
-        return (int) (age * 365.242199 * 27);
+    private float bathroomTimeMinutes(LocalDate birthDate, int age) {
+        float timeDay = 14.5f;
+        float timeMobileModifier = 12.5f;
+        float totalBathroomTime = 0;
+        LocalDate date = birthDate;
+        while(!date.equals(birthDate.withYear(birthDate.getYear()+age))) {
+            totalBathroomTime += (date.isBefore(birthDate.withYear(2017)))? timeDay:timeDay+timeMobileModifier;
+            date = date.plusDays(1);
+        }
+        return totalBathroomTime;
+    }
+
+    private float freeTimeHours(LocalDate birthDate, int age, boolean mobileUse) {
+        float sleep = 8;
+        float work  = 9; // not on weekends or after 65
+        float other = (24*0.02f)+2; // double after 65
+        float hoursFreeTime = 0;
+        LocalDate date = birthDate;
+        while(!date.equals(birthDate.withYear(birthDate.getYear()+age))) {
+            if(!date.isBefore(birthDate.withYear(birthDate.getYear()+65))) { // age 65 and up
+                hoursFreeTime += 24-(other*2+sleep);
+            } else { // before age 65
+                if (date.getDayOfWeek()==DayOfWeek.SATURDAY || date.getDayOfWeek()==DayOfWeek.SUNDAY) {
+                    hoursFreeTime += 24-(sleep+other);
+                } else {
+                    hoursFreeTime += 24-(work+sleep+other);
+                }
+            }
+            if(mobileUse) hoursFreeTime -= 3;
+            date = date.plusDays(1);
+        }
+        return hoursFreeTime;
+    }
+
+    private void prints() {
+        System.out.println("Days passed since my birthday: " + dateDifferenceDays(LocalDate.now(), birthDate));
+        System.out.println("Skype meeting in New York at: " + DateTimeFormatter.ofPattern("MM-dd-yyyy hh:mm a").withLocale(new Locale("en", "US")).format(skypeAppointmentNewYork));
+        System.out.println("Skype meeting in Malmö at: " + DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm").format(skypeAppointmentMalmo));
+        System.out.println("Number of Friday the Thirteenths: " + fridayThirteenth(LocalDate.of(1900, 1, 13)));
+        System.out.println("Antal dagar sittandes på toaletten till ålder 80: "+bathroomTimeMinutes(birthDate,30)/60/24);
+        System.out.printf("Total fritid i procent av ens liv vid ålder 80: %.2f%%\n",(freeTimeHours(birthDate, 80, false)/24/30.4368499/12)/80*100);
+        System.out.println("Total fritid i timmar fram till ålder 80: "+freeTimeHours(birthDate, 80, false));
+        System.out.println("Total fritid i timmar fram till ålder 80 med mobiltid borträknat: "+freeTimeHours(birthDate, 80, true));
     }
 }
